3f9bb51986938048d4e35c34a7247ff790bc6b1d
anmfun <anmfun@gmail.com>
2020-07-24 20:27:39 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gh-pages-react/precache-manifest.2e45e551dc96b74e920eb47f0b342267.js"
+  "/gh-pages-react/precache-manifest.d2c5f352694abd55f202ee565a4ca4f7.js"
 );
 
 self.addEventListener('message', (event) => {
